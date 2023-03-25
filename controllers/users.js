f32c4df4ed0d67c834ec6cf9cc4db44839866d70
ericcnguyen04 <ericc.win@gmail.com>
2023-03-24 20:44:33 -0400

    process of creating registration

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 // required packages
 const router = require('express').Router()
-// database
+const db = require('../models') // database
 const bcrypt = require('bcryptjs') // method to hash and compare passwords
 const jwt = require('jsonwebtoken') // used for authentication and authorization
 
@@ -9,6 +9,18 @@ const jwt = require('jsonwebtoken') // used for authentication and authorization
 // GET /users - test endpoint
 router.get('/', (req, res) => {
     res.json({ msg: 'welcome to the users endpoint' })
+})
+
+// POST /users/register - create a new user
+router.post('/register', async (req, res) => {
+    try {
+        // checking if user exist already
+        const findUser = await db
+
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({ msg: 'server error' })
+    }
 })
 
 // export
