19eef6d617edd569c1e851e03a141ade206c9715
ericcnguyen04 <ericc.win@gmail.com>
2023-03-24 21:18:21 -0400

    made models

---
+++ models/index.js
@@ -0,0 +1,29 @@
+// require mongoose package
+const mongoose = require("mongoose")
+require('dotenv').config()
+
+
+const dbName = 'nephData'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/' + dbName
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+// Connection methods
+db.once('open', () => {
+  console.log(`🔗 Connected to MongoDB at ${db.host}:${db.port}`)
+})
+
+db.on('error',  err => {
+  console.error(`🔥 Datacenter burned down:\n${err}`)
+})
+
+
+module.exports = {
+  User: require('./User'),
+  Item: require('./Item')
+}
