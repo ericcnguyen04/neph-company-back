3834ccfa4856e333d427c9044c81ec20fab6dedb
ericcnguyen04 <ericc.win@gmail.com>
2023-03-25 23:41:18 -0400

    show all for post controller

--- controllers/post.js
+++ controllers/post.js
@@ -6,13 +6,26 @@ const authLockedRoute = require('./api-v1/authLockedRoute')
 
 // mount our routes on the router
 
-// GET /:id -- get one item
-router.get('/:id', async (req,res) =>{
+// GET /:id -- get one post
+router.get('/:id', async (req, res) =>{
     try{
-        const item = await db.Item.findOne({
+        const post = await db.Post.findOne({
             _id: req.params.id
         })
-        res.json(item)
+        res.json(post)
+    } catch(err){
+        console.log(err)
+        res.status(500).json({
+            msg: 'internal server error, contact the system administrator'
+        })
+    }
+})
+
+// GET / -- display all items
+router.get('/', async (req, res) =>{
+    try{
+        const post = await db.Post.find()
+        res.json(post)
     } catch(err){
         console.log(err)
         res.status(500).json({
