b3fd612e13ac5778e035d03d113c524ae0b5ba46
ericcnguyen04 <ericc.win@gmail.com>
2023-03-25 23:47:32 -0400

    making post controllers more

--- controllers/post.js
+++ controllers/post.js
@@ -34,3 +34,36 @@ router.get('/', async (req, res) =>{
     }
 })
 
+// POST /post/new -- create one post 
+router.post('/new', authLockedRoute, async (req,res) =>{
+    try{
+        // find user
+        const existingUser = res.locals.user
+
+    // creates a new item
+    const newPost = await db.Item.findOneAndUpdate( 
+        {title: req.body.title} ,
+        {caption: req.body.caption , 
+        picture: req.body.picture , 
+        url: req.body.url } ,
+        //upsert is an update or create command
+        {upsert: true, new: true}
+    )
+
+    //associates user with item
+    existingUser.post.push(newPost._id)
+    await existingUser.save()
+
+    // associates item with a user
+    newPost.userId = existingUser._id
+    await newPost.save()
+
+    res.json({msg: 'user created item'})
+
+    } catch(err){
+        console.log(err)
+        res.status(500).json({
+            msg: 'internal server error, contact the system administrator'
+        })
+    }
+})
